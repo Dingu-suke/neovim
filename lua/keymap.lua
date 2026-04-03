accdd7d265fe5151a604e44192f667c72ec4add9
Dingu-suke <e2310000rubik@gmail.com>
2026-04-04 00:32:01 +0900

    refactor: keymap.lua の未使用変数を削除

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,5 +1,3 @@
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
 -- Undo/Redo with Command key (macOS)
 vim.keymap.set('n', '<D-z>', 'u', { noremap = true, silent = true })
 vim.keymap.set('n', '<D-Z>', '<C-r>', { noremap = true, silent = true })
